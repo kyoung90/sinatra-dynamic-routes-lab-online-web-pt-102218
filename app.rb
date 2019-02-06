6cdcbac4c78ef47c7378e5b8f1277f54deb51af1
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-06 16:37:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-  get "/reversename/:name" do 
+  get "/reversename/:name" do
     @name = params[:name]
     "#{@name.reverse}"
-  end 
+  end
 end

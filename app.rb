3ab57d9d9bf5bdbe2441718c42254bcd95666542
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-06 16:45:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,13 +33,19 @@ class App < Sinatra::Base
 # (add, subtract, multiply or divide)
   get "/:operation/:number1/:number2" do
     @operation = params[:operation]
-    @number1 = params[:number1]
-    @number2 = params[:number2]
+    @number1 = params[:number1].to_i
+    @number2 = params[:number2].to_i
 
     if operation == "add"
+      "#{@number1 + @number2}"
     elsif operation == "subtract"
+      "#{@number1 - @number2}"
     elsif operation == "multiply"
+      "#{@number1 * @number2}"
     elsif operation == "divide"
+      "#{@number1 / @number2}"
+    else 
+      "Operation not understood"
     end
   end
 end

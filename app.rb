6e0475333cf8f20513ff052b9d7ed150d9c2216a
Kenneth Young Castro <kenneth.young90@gmail.com>
2019-02-06 16:50:50 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,9 +15,11 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
     @number = params[:number].to_i
     @phrase = params[:phrase]
+    str = ""
     @number.times do
-      "#{@phrase}"
+      str += "#{@phrase} "
     end
+    str
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
